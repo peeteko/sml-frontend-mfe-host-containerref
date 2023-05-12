074a65f3f60fdf1c73ba8e3fdd30d163c0e42281
peeteko <koen.peeters@bpost.be>
2023-05-12 07:48:37 +0200

    removed console.log

--- src/app/directives/host-remote.directive.ts
+++ src/app/directives/host-remote.directive.ts
@@ -11,30 +11,25 @@ export class HostRemoteDirective {
   
 
   constructor( private viewContainerRef : ViewContainerRef, private readonly userService : UserService, private changeDetectorRef : ChangeDetectorRef) { 
-    console.log('HostRemoteDirective contstructor');
-
+  
   }
 
   ngOnInit() : void {
-    console.log('HostRemoteDirective ngOnInit');
-    this.userService.smlUserDetailsWithTokenSubject.subscribe((smlUserDetailsWithToken : SmlUserDetailsWithToken | null) => {
+     this.userService.smlUserDetailsWithTokenSubject.subscribe((smlUserDetailsWithToken : SmlUserDetailsWithToken | null) => {
       if (smlUserDetailsWithToken === null){
-        console.log('clear ref in onInit');
-        this.viewContainerRef.clear();
+         this.viewContainerRef.clear();
       }
       else{
         this.loadRemoteModuleAndSetToken();
-      }      
+      }
     })
 
     this.loadRemoteModuleAndSetToken();
-   
-
-   
+ 
   }
 
   private loadRemoteModuleAndSetToken() : void{
-    console.log('loadRemoteModuleAndSetToken');
+   
     loadRemoteModule({
       type : 'module',
       remoteEntry : 'http://localhost:4201/remoteEntry.js',
@@ -43,9 +38,7 @@ export class HostRemoteDirective {
       const componentType = m.PluginConnectionsComponent;      
       const componentInstance = this.viewContainerRef.createComponent(componentType).instance;
       (componentInstance as any).jwtTokenCurrentValue = this.userService.smlUserDetailsWithToken?.jwtTokenCustomized;
-      console.log('token in remote component' + (componentInstance as any).jwtTokenCurrentValue);
-      console.log('this.userService.smlUserDetailsWithToken' + this.userService.smlUserDetailsWithToken);
-    }).catch(e => {
+     }).catch(e => {
       console.error(e);
       return import('../epf-placeholder/epf-placeholder.module').then(m => m.EpfPlaceHolderModule);
     })
